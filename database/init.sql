8d692190af356ec06edb475756a1553c44759923
Ofomata <Ofomataphilip@gmail.com>
2025-11-05 14:11:49 +0000

    added a postgres initialization script to database folder

--- database/init.sql
+++ database/init.sql
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS products (
 
 -- sample products
 INSERT INTO products (name, description, price, stock, image) VALUES
-('Laptop', 'High-performance laptop', 949.99, 10, '/images/laptop.jpg'),
-('Smartphone', 'Latest smartphone model', 699.99, 22, '/images/smartphone.jpg'),
-('Headphones', 'Wireless noise-cancelling headphones', 199.99, 10, '/images/headphones.jpg'),
+('Laptop', 'High-performance laptop', 949.99, 10, '/Images/laptop.jpg'),
+('Smartphone', 'Latest smartphone model', 699.99, 22, '/Images/smartphone.jpg'),
+('Headphones', 'Wireless noise-cancelling headphones', 199.99, 10, '/Images/headphone.jpg'),
 ('Keyboards', 'Mechanical Keyboard with RGB lights', 149.99, 20, '/Images/keyboard.jpg'),
 ('Gaming Mouse', 'Wireless Gaming mouse with RGB lights', 99.99, 10, '/Images/mouse.jpg'),
 ('Tablets', 'Latest tablet model', 299.99, 5, '/Images/tablet.jpg'),
-('Airpods', 'Original Apple Airpods', 149.99, 20, '/images/airpods.jpg');
+('Airpods', 'Original Apple Airpods', 149.99, 20, '/Images/airpods.jpg');
