2e075f8c5012a87281da451ec9097405f129460e
Ofomata <Ofomataphilip@gmail.com>
2025-12-13 07:35:15 +0000

    updated charger extension filename in database

--- database/init.sql
+++ database/init.sql
@@ -11,11 +11,11 @@ CREATE TABLE IF NOT EXISTS products (
 
 -- sample products
 INSERT INTO products (name, description, price, stock, image) VALUES
-('Laptop', 'High-performance laptop', 949.99, 10, '/Images/laptop.jpg'),
-('Smartphone', 'Latest smartphone model', 699.99, 22, '/Images/smartphone.jpg'),
+('Laptops', 'High-performance laptop', 949.99, 10, '/Images/laptop.jpg'),
+('Smartphones', 'Latest smartphone model', 699.99, 22, '/Images/smartphone.jpg'),
 ('Headphones', 'Wireless noise-cancelling headphones', 199.99, 10, '/Images/headphone.jpg'),
 ('Keyboards', 'Mechanical Keyboard with RGB lights', 149.99, 20, '/Images/keyboard.jpg'),
 ('Gaming Mouse', 'Wireless Gaming mouse with RGB lights', 99.99, 10, '/Images/mouse.jpg'),
 ('Tablets', 'Latest tablet model', 299.99, 5, '/Images/tablet.jpg'),
 ('Airpods', 'Original Apple Airpods', 149.99, 20, '/Images/airpods.jpg'),
-('Chargers', 'Fast Charging Phone Charger', 49.99, 15, '/Images/charger.avif');
+('Chargers', 'Fast Charging Phone Charger', 49.99, 15, '/Images/charger.jpg');
